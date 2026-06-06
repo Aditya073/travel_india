99d96f75bc782496c037f0b63810e674eaf05b74
Aditya073 <adityarajendradixit@gmail.com>
2026-06-06 17:06:52 +0530

    Updated the card database

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:travel_india/Config/Theme/app_theme.dart';
 import 'package:travel_india/features/auth/data/datasources/FirebaseAuthDataSource.dart';
 import 'package:cloudinary_flutter/cloudinary_context.dart';
-// import 'package:cloudinary_flutter/image/cld_image.dart';
 import 'package:cloudinary_url_gen/cloudinary.dart';
 import 'package:travel_india/features/auth/data/repositories/auth_repoImpl.dart';
 import 'package:travel_india/features/auth/domain/usecases/loginUseCase.dart';
 import 'package:travel_india/features/auth/presentation/bloc/auth_bloc.dart';
-// import 'package:travel_india/features/auth/presentation/pages/login_page.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:travel_india/features/auth/data/datasources/firebase_options.dart';
 import 'package:travel_india/features/home_page/data/datasources/firestoreDataSource.dart';
@@ -17,7 +15,7 @@ import 'package:travel_india/features/home_page/domain/usecases/getCardData.dart
 import 'package:travel_india/features/home_page/presentation/bloc/card_bloc.dart';
 import 'package:travel_india/features/home_page/presentation/pages/home_page.dart';
 
-Future<void> main() async { 
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   // ignore: deprecated_member_use
