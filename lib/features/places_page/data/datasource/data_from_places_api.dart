20bc424ed923d096494c3953c8ad6bc215433c89
Aditya073 <adityarajendradixit@gmail.com>
2026-07-12 16:40:44 +0530

    working with api for Places

--- lib/features/places_page/data/datasource/data_from_places_api.dart
+++ lib/features/places_page/data/datasource/data_from_places_api.dart
@@ -1,3 +1,84 @@
-class DataFromPlacesApi {
-  
-}
+class DataFromPlacesApi {}
+
+/*
+1.         ***********************************************************************************************
+[out:json][timeout:120];
+
+area["name"="Maharashtra"]["boundary"="administrative"]->.searchArea;
+
+(
+  node["natural"="beach"](area.searchArea);
+  way["natural"="beach"](area.searchArea);
+  relation["natural"="beach"](area.searchArea);
+);
+
+out center tags;
+
+
+2.         ***********************************************************************************************
+[out:json][timeout:120];
+
+area["name"="Maharashtra"]["boundary"="administrative"]->.searchArea;
+
+(
+  node["tourism"="zoo"](area.searchArea);
+  way["tourism"="zoo"](area.searchArea);
+  relation["tourism"="zoo"](area.searchArea);
+);
+
+out center tags;
+
+
+3.        ***********************************************************************************************
+[out:json][timeout:120];
+
+area["name"="Maharashtra"]["boundary"="administrative"]->.searchArea;
+
+(
+  node["waterway"="waterfall"](area.searchArea);
+  way["waterway"="waterfall"](area.searchArea);
+  relation["waterway"="waterfall"](area.searchArea);
+);
+
+out center tags;
+
+
+4.         ***********************************************************************************************
+  [out:json][timeout:120];
+
+area["name"="Maharashtra"]->.searchArea;
+
+(
+  node["historic"](area.searchArea);
+);
+
+out center tags;
+
+
+
+5.         ***********************************************************************************************
+  [out:json][timeout:120];
+
+area["name"="Maharashtra"]->.searchArea;
+
+(
+  node["leisure"](area.searchArea);    ---> parks
+);
+
+out center tags;
+
+
+
+6.         ***********************************************************************************************
+[out:json][timeout:120];
+
+area["name"="Maharashtra"]["boundary"="administrative"]->.searchArea;
+
+(
+  node["tourism"="museum"](area.searchArea);
+  way["tourism"="museum"](area.searchArea);
+  relation["tourism"="museum"](area.searchArea);
+);
+
+out center tags;
+*/
