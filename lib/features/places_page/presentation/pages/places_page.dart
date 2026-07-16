1960404acfd6a23272652cd5ee5525f54e3887cb
Aditya073 <adityarajendradixit@gmail.com>
2026-07-16 07:41:17 +0530

    working on the UI part for Places feature of the app

--- lib/features/places_page/presentation/pages/places_page.dart
+++ lib/features/places_page/presentation/pages/places_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:travel_india/Config/Theme/app_theme.dart';
 
@@ -12,30 +13,36 @@ class PlacesPage extends StatefulWidget {
 class _PlacesPageState extends State<PlacesPage> {
   @override
   Widget build(BuildContext context) {
+    Set<String> categoriesOfPlaces = {
+      'Waterfall',
+      'Historic',
+      'Beach',
+      'Zoo',
+      'Museum',
+      'Leisure',
+    };
     return Scaffold(
       body: Column(
         children: [
-
           // UPPER Container
           Container(
-            decoration: BoxDecoration(
-              color: AppTheme.darkColor,
-            ),
+            decoration: BoxDecoration(color: AppTheme.darkColor),
             child: SafeArea(
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
 
                 children: [
                   Padding(
-                    padding: const EdgeInsets.fromLTRB(10, 0, 8, 10),
+                    padding: const EdgeInsets.fromLTRB(10, 0, 8, 0),
                     child: Text(
                       'Explore',
                       style: TextStyle(
-                        fontSize: 20,
+                        fontSize: 18,
                         fontWeight: FontWeight.w900,
-                        color: AppTheme.iceBlue,
+                        color: AppTheme.powderBlue,
                       ),
-                    ),),
+                    ),
+                  ),
                   Padding(
                     padding: const EdgeInsets.fromLTRB(10, 0, 8, 10),
                     child: Text(
@@ -74,7 +81,10 @@ class _PlacesPageState extends State<PlacesPage> {
                         child: const TextField(
                           // ************ this should show the only results that is being typed ***************************************
                           decoration: InputDecoration(
-                            prefixIcon: Icon(Icons.search, color: AppTheme.powderBlue),
+                            prefixIcon: Icon(
+                              Icons.search,
+                              color: AppTheme.powderBlue,
+                            ),
                             hintText: "Search restaurants, cafas...",
                             hintStyle: TextStyle(
                               color: AppTheme.powderBlue,
@@ -83,6 +93,80 @@ class _PlacesPageState extends State<PlacesPage> {
                             border: InputBorder.none,
                           ),
                         ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+
+          // upper Row
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(color: AppTheme.powderBlue),
+
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: SizedBox(
+                      height: 70,
+                      child: ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: categoriesOfPlaces.length,
+                        itemBuilder: (context, index) {
+
+                          final category = categoriesOfPlaces.elementAt(index);
+                          final Map<String, IconData> iconMap = {
+                            'Waterfall': Icons.water_drop_outlined,
+                            'Historic': Icons.fort_outlined,
+                            'Beach': Icons.beach_access,
+                            'Zoo': Icons.pets,
+                            'Museum': Icons.museum,
+                            'Leisure': Icons.sports_soccer,
+                          };
+                          
+                          return Column(
+                            children: [
+                              Container(
+                                padding: EdgeInsets.all(10),
+                                margin: EdgeInsets.symmetric(
+                                  horizontal: 18,
+                                  vertical: 5,
+                                ),
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(20),
+                                  // border: Border.all(
+                                  //   style: BorderStyle.solid,
+                                  //   width: 1,
+                                  // ),
+                                ),
+
+                                child: Center(
+                                  child: Icon(
+                                    iconMap[category] ?? Icons.help_outline,
+                                    size: 35,
+                                    color: AppTheme.primaryColor,
+                                  ),
+                                ),
+                              ),
+
+                              // Padding(
+                              //   padding: const EdgeInsets.fromLTRB(2, 8, 2, 3),
+                              // child:
+                              // Text(
+                              //   category,
+                              //   style: TextStyle(
+                              //     color: Colors.grey,
+                              //     fontSize: 5,
+                              //   ),
+                              // ),
+                              // ),
+                            ],
+                          );
+                        },
                       ),
                     ),
                   ),
